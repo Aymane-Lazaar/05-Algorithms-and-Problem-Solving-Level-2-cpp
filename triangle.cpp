b3166d8ad2cfc09b03eb6a8454832ccfe4ed3b15
aymane lazaar <aymanelazaar5@gmail.com>
2025-12-27 06:05:52 +0100

    Restoring all files to the repository and cancelling the previous deletion attempt.

---
+++ triangle.cpp
@@ -0,0 +1,264 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// void AddArray(int array[6],int number,int length)
+// {
+//     int index=length;
+//     for (int i = 0; i <= length; i++)
+//     {
+//         array[index]=number;
+//         cout<<array[index];
+//     }
+// }
+
+int main()
+{
+
+    // int array[6];
+
+    // int arr[6]={3, 2, 6, 2, 1, 3};
+    // int length=0;
+    
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     for (int j = i; j < 6; j++)
+    //     {
+    //         if (arr[i]==arr[j]){
+            
+    //             array[i]=arr[i];
+    //             cout<<array[i]<<endl;
+    //             }// break;
+    //     }
+        
+            
+    // }
+    
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cout<<array[i]<<endl;
+    // }
+    
+
+    // int n=9;
+    // cout<<endl;
+    /*
+    //ex1
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     cout<<"\t\t";
+    //     for (int j = n; j > i; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+
+    //     for (int k = 0; k < 2*i+1; k++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // cout<<endl<<endl;
+    // //ex2
+    // for (int i = n; i >= 0 ; i--)
+    // {
+    //     cout<<"\t\t";
+    //     for (int j = n; j > i; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+
+    //     for (int k = 0; k < 2*i+1; k++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // //ex3
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout<<"\t\t";
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+    // cout<<endl<<endl;
+
+    // //ex4
+    // for (int i = 10; i > 0; i--)
+    // {
+    //     cout<<"\t\t";
+    //     for (int j = i; j > 0; j--)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+    
+    // cout<<endl<<endl;
+    // //ex5
+
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     cout<<"\t\t";
+    //     for (int j = n; j > i; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+
+    //     for (int k = 0; k <= i; k++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // cout<<endl<<endl;
+    // //ex6
+    
+    // for (int i = n; i >=0; i--)
+    // {
+    //     cout<<"\t\t";
+    //     for (int j = n; j > i; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+
+    //     for (int k = 0; k <= i; k++)
+    //     {
+    //         cout<<"*";
+    //     }bbakhvdk
+    //     cout<<endl;
+    // }
+*/
+
+    /*int sum=0,stock=0,result=0;
+    
+    int num=10;
+
+    for (int i = 1; i < num; i++)
+    {
+        // if (num<0)
+        // {
+            for (int j = 1; j < num; j++)
+            {
+                sum=j;
+            }
+            
+        // }
+
+        // stock=i
+        
+        
+    }
+    cout<<sum;
+    */
+
+    /*for (int i = 1; i <= 4; i++) {
+
+    for (int j = 4; j > i; j--) {
+        cout<<" ";
+    }
+
+    for (int k = 1; k <= 2*i-1; k++) {
+        cout<<"*";
+    }
+    
+    cout<<endl;
+    if (i==4)
+    {
+        for (int j = 1; j < i ; j++) {
+        cout<<" ";
+    }
+        cout<<"|";
+    }
+
+    }*/
+
+
+    // int Money;
+    // cout<<"Enter Money: ";
+    // cin>>Money;
+    // // int Remainder+=Money;
+    // int HowMany200Dh=Money/200;
+    // int Remainder+=how;
+    // cout<<"200 existe ";
+    
+
+    
+    // int arr[6]={1,2,3,1,2,1};
+    // int Checked[6];
+    // int length=0;
+
+/*   
+
+for (short i = 0; i < 6; i++)
+{
+    int Counter=0;
+
+    for (short j = 0; j < 6; j++)
+    {
+        if (i==j)
+
+            continue;  
+
+        if(arr[i]==arr[j] ){
+            Counter++;
+        }
+          
+    }
+        if (Counter>=1 )
+        {
+            cout<<"number "<<arr[i]<<" repeted: "<<Counter+1<<" Time(s)\n";
+        }
+}
+    
+       
+
+    */
+ 
+ 
+/*    for (short i = 0; i < 6; i++)
+    {
+        int Counter=0;
+        for (short j = 0; j < 6; j++)
+        {
+            // if (arr[i]==arr[j])
+            if (i==j)
+            {
+                continue;
+            }
+
+            if(arr[i]!=arr[j])
+            {
+                length++;
+                arr2[i]=arr[j];
+                break;
+                // Counter++;
+            }
+            
+        }
+
+           
+        if (Counter>=1 )
+            cout<<"number "<<arr[i]<<" repeted: "<<++Counter<<" Time(s)\n";
+    }
+
+    for (short i = 0; i < length; i++)
+    {
+        cout<<arr2[i]<<endl;
+    }
+    */
+    
+    
+    
+
+    return 0;
+}
